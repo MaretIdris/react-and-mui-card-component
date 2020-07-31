b544ebd85a5a9aed86167ede27ee908ec665a6bd
Maret Idris <maret@fasterlap.com>
2020-07-30 17:09:18 -0700

    Add layout styling and favicon

--- src/components/LocationImage.js
+++ src/components/LocationImage.js
@@ -4,11 +4,13 @@ class LocationImage extends Component {
     render() {
         const index = this.props.selectedLocationIndex;
         // If index has not been generated yet, return nothing.
-        if (index === undefined) return (null);
-        const { location, image } = this.props.arrayOfLocations[index];
+        if (index === undefined) return null;
+        const {location, image} = this.props.arrayOfLocations[index];
 
         return (
-            <img src={image} alt={location}/>
+            <div className="image-container">
+                <img src={image} alt={location}/>
+            </div>
         );
     }
 }
--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -4,15 +4,15 @@ class WeatherData extends Component {
     render() {
         const index = this.props.selectedLocationIndex;
         // If index has not been generated yet, return nothing.
-        if(index === undefined) return (null);
-        const { location, tempInC, feelsLike, airQualityIndex} = this.props.arrayOfLocations[index];
+        if (index === undefined) return null;
+        const {location, tempInC, feelsLike, airQualityIndex} = this.props.arrayOfLocations[index];
 
         return (
-            <div>
+            <div className="weather-text-container">
                 <h1>{location}</h1>
-                <p>{tempInC}°C</p>
-                <p>{feelsLike}</p>
-                <p>Air Quality: {airQualityIndex}</p>
+                <p>- {tempInC}°C</p>
+                <p>- {feelsLike}</p>
+                <p>- Air Quality: {airQualityIndex}</p>
             </div>
         );
     }
